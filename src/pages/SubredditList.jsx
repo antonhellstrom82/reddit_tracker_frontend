dc287bc99a91ce4c9c9806e500894c7a88de9955
antonhellstrom82 <anton.hellstrom@gmail.com>
2025-02-08 23:23:45 +0100

    Update SubredditList.jsx

--- src/pages/SubredditList.jsx
+++ src/pages/SubredditList.jsx
@@ -1,1 +1,26 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const SubredditList = () => {
+  const [subreddits, setSubreddits] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://reddit-tracker-jzk5.onrender.com/api/subreddits")
+      .then(response => setSubreddits(response.data.subreddits))
+      .catch(error => console.error("Error fetching data:", error));
+  }, []);
+
+  return (
+    <div>
+      <h2>Övervakade subreddits</h2>
+      <ul>
+        {subreddits.map((sub, index) => (
+          <li key={index}>{sub}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default SubredditList;
 
