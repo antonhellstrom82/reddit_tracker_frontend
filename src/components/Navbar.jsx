4b053673f596e64e9013a3ef381b66b3e66b7108
antonhellstrom82 <anton.hellstrom@gmail.com>
2025-02-08 23:24:20 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li><Link to="/">Hem</Link></li>
+        <li><Link to="/activity-chart">Aktivitetsgraf</Link></li>
+        <li><Link to="/subreddits">Subreddits</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
 
