cc4785241276bd82b1070e395ab1174afbb1342a
antonhellstrom82 <anton.hellstrom@gmail.com>
2025-02-08 23:21:25 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,22 @@
-Tillfälligt
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import ActivityChart from "./pages/ActivityChart";
+import SubredditList from "./pages/SubredditList";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="app">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/activity-chart" element={<ActivityChart />} />
+        <Route path="/subreddits" element={<SubredditList />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
+
