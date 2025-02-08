d62ae3577bd57f0c4f8caefbff9c261a99b01572
antonhellstrom82 <anton.hellstrom@gmail.com>
2025-02-08 23:22:26 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,1 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Reddit Tracker</h1>
+      <p>Övervaka aktiva användare i olika subreddits.</p>
+    </div>
+  );
+};
+
+export default Home;
 
